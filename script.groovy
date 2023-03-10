48742e60c33e5e79be963f8959bf8edd91c75168
Umesh Suryavanshi <39472741+umesh537@users.noreply.github.com>
2023-03-10 23:06:22 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,16 +1,16 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
+// def buildJar() {
+//     echo "building the application..."
+//     sh 'mvn package'
+// } 
 
-def buildImage() {
-    echo "building the docker image"
-        withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]){
-            sh 'docker build -t umeshsurya01/demo-app:jma-2.0 .'
-            sh 'echo $PASS | docker login -u $USER --password-stdin'
-            sh 'docker push umeshsurya01/demo-app:jma-2.0'
-    }
-} 
+// def buildImage() {
+//     echo "building the docker image"
+//         withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]){
+//             sh 'docker build -t umeshsurya01/demo-app:jma-2.0 .'
+//             sh 'echo $PASS | docker login -u $USER --password-stdin'
+//             sh 'docker push umeshsurya01/demo-app:jma-2.0'
+//     }
+// } 
 
 def deployApp() {
     echo 'deploying the application...'
