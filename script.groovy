bfc5b62474e61dbe21de33e61039f47315f7a741
Umesh Suryavanshi <39472741+umesh537@users.noreply.github.com>
2023-07-02 00:56:00 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image"
-        withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]){
+        withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'USER', passwordVariable: 'PASS')]){
             sh 'docker build -t umeshsurya01/demo-app:jma-2.0 .'
             sh 'echo $PASS | docker login -u $USER --password-stdin'
             sh 'docker push umeshsurya01/demo-app:jma-2.0'
